05e6fd71047c273215632cc01acb82b565105d4b
chris3evans <chris3evans.ce@gmail.com>
2023-01-20 10:09:16 +0000

    [Feature] - added basic binding to workout form event handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WorkoutsViewComponent } from './workouts-view/workouts-view.component';
 import { WorkoutFormComponent } from './workout-form/workout-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WorkoutsViewComponent,
-    WorkoutFormComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, WorkoutsViewComponent, WorkoutFormComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/workout-form/workout-form.component.ts
+++ src/app/workout-form/workout-form.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-workout-form',
   templateUrl: './workout-form.component.html',
-  styleUrls: ['./workout-form.component.css']
+  styleUrls: ['./workout-form.component.css'],
 })
 export class WorkoutFormComponent {
+  constructor(private formBuilder: FormBuilder) {}
+  workoutForm = this.formBuilder.group({
+    workoutName: '',
+    workoutType: '',
+  });
 
+  ngOnInit(): void {
+    this.workoutForm.valueChanges.subscribe((result) => {
+      console.log(result);
+    });
+  }
 }
