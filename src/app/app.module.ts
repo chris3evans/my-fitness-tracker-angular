a5517eb4f456926068e6375a8bbd6438c258bb18
chris3evans <chris3evans.ce@gmail.com>
2023-01-20 09:19:18 +0000

    [Feature] - built HTML for workouts form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { WorkoutsViewComponent } from './workouts-view/workouts-view.component';
+import { WorkoutFormComponent } from './workout-form/workout-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WorkoutsViewComponent
+    WorkoutsViewComponent,
+    WorkoutFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/workout-form/workout-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-form',
+  templateUrl: './workout-form.component.html',
+  styleUrls: ['./workout-form.component.css']
+})
+export class WorkoutFormComponent {
+
+}
